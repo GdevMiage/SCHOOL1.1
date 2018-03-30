05349aae9aa426315493a6bf274e56983677b6ff
wadi2a <ben.aissa.ouadie@gmail.com>
2018-03-30 02:56:43 +0200

    modification architecture generale

--- src/gestionetablissement/controleur/FXMLGEtablissementController.java
+++ src/gestionetablissement/controleur/FXMLGEtablissementController.java
@@ -11,8 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-idIndividus
-indivius.getNom()
+
 /**
  *
  * @author Ouadie
