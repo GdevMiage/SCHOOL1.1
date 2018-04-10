592e99a3d937338eb9817907e644bd0635a2c470
salimaouali <salimaouali08@gmail.com>
2018-04-11 01:36:23 +0200

    matieres : integration  of  definirId, liste static

--- src/gestionetablissement/modele/Matieres.java
+++ src/gestionetablissement/modele/Matieres.java
@@ -1,6 +1,10 @@
  
 package gestionetablissement.modele;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+
  
 public class Matieres {
     
@@ -8,6 +12,48 @@ public class Matieres {
     private Modules module;
     private String libelMatiere;
 
+     public static final ListPro<Matieres> listeMatieres = new ListPro<Matieres>();
+   
+    {
+        this.listeMatieres.add(this);
+    }
+  
+        //pour definir a chaque fois les ID
+    public static int definirId(){
+        Iterator it = listeMatieres.iterator();
+        
+        //année actuelle 
+       
+        int annee =Calendar.getInstance().get(Calendar.YEAR);
+        
+        Matieres MatieresPlusGrandId=null;
+        boolean vide =true;
+        if(it.hasNext())  MatieresPlusGrandId=  (Matieres) it.next();
+        while(it.hasNext()){
+            Matieres Matieres = (Matieres) it.next();
+            vide=false;
+            if(Matieres.getIdMatiere()>MatieresPlusGrandId.getIdMatiere()) MatieresPlusGrandId =Matieres;
+        }
+            
+            if(vide) {System.out.println((annee *10)+1);return ((annee *100000)+1);}
+            else {System.out.println(MatieresPlusGrandId.getIdMatiere()+1);return MatieresPlusGrandId.getIdMatiere()+1;}
+        
+        
+    }
+    
+    public static ArrayList<Matieres> of(String id ){
+        Iterator it = listeMatieres.iterator();
+        ArrayList<Matieres> listRecherche=null;
+        while(it.hasNext()){
+            Matieres Matieres = (Matieres) it.next();
+            String idd = Matieres.getIdMatiere()+"";
+            if(idd.indexOf(id)!=0){
+                
+                listRecherche.add(Matieres);
+            }
+        }
+        return listRecherche;
+    }
     public Matieres(int idMatiere, Modules module, String libelMatiere) {
         this.idMatiere = idMatiere;
         this.module = module;
