ce9441ca035180480286a103d34eeac2d52e105e
salimaouali <salimaouali08@gmail.com>
2018-04-11 00:55:50 +0200

    etudiants, entretien:  liste static

--- src/gestionetablissement/modele/Entretien.java
+++ src/gestionetablissement/modele/Entretien.java
@@ -8,6 +8,12 @@ import java.util.Date;
 public class Entretien extends Individus {
     
     private ListPro<Entretien> listeAgentEntretien;
+    
+    public static final ListPro<Entretien> listeEntretien = new ListPro<Entretien>();
+   
+    {
+        this.listeEntretien.add(this);
+    }
 
 
     public Entretien( String nomIndividu, String prenomIndividu, String genreIndividu, Date datNaisIndividu, String lieuNaisIndividu, String nivEtudIndividu, String domEtudIndividu, String profIndividu, Adresses domCompetIndividu, String langMatIndividu, String langParlIndividu, String photoIndividu, String rectoPIDIndividu, String versoPIDIndividu,String tel,String mail, Affectations affectation) {
