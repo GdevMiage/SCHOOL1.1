550d533f7c43947e6ce943988b419fe4213aac13
salimaouali <salimaouali08@gmail.com>
2018-04-11 01:25:54 +0200

    groupe,horraire : integration  of  definirId

--- src/gestionetablissement/modele/Horaires.java
+++ src/gestionetablissement/modele/Horaires.java
@@ -1,7 +1,10 @@
 
 package gestionetablissement.modele;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 
 
 public class Horaires {
@@ -12,7 +15,51 @@ public class Horaires {
   private Date debut;
   private Date fin;
   private String libelHoraire;
-  private ListPro<Horaires> listeHoraire;
+  
+  
+  
+  public static final ListPro<Horaires> listeHoraire = new ListPro<Horaires>();
+   
+    {
+        this.listeHoraire.add(this);
+    }
+  
+        //pour definir a chaque fois les ID
+    public static int definirId(){
+        Iterator it = listeHoraire.iterator();
+        
+        //année actuelle 
+       
+        int annee =Calendar.getInstance().get(Calendar.YEAR);
+        
+        Horaires HorairesPlusGrandId=null;
+        boolean vide =true;
+        if(it.hasNext())  HorairesPlusGrandId=  (Horaires) it.next();
+        while(it.hasNext()){
+            Horaires Horaires = (Horaires) it.next();
+            vide=false;
+            if(Horaires.getIdHoraire()>HorairesPlusGrandId.getIdHoraire()) HorairesPlusGrandId =Horaires;
+        }
+            
+            if(vide) {System.out.println((annee *10)+1);return ((annee *100000)+1);}
+            else {System.out.println(HorairesPlusGrandId.getIdHoraire()+1);return HorairesPlusGrandId.getIdHoraire()+1;}
+        
+        
+    }
+    
+    public static ArrayList<Horaires> of(String id ){
+        Iterator it = listeHoraire.iterator();
+        ArrayList<Horaires> listRecherche=null;
+        while(it.hasNext()){
+            Horaires Horaires = (Horaires) it.next();
+            String idd = Horaires.getIdHoraire()+"";
+            if(idd.indexOf(id)!=0){
+                
+                listRecherche.add(Horaires);
+            }
+        }
+        return listRecherche;
+    }
   
     public Horaires(){
         
@@ -34,8 +81,7 @@ public class Horaires {
         this.debut = debut;
         this.fin = fin;
         this.libelHoraire = libelHoraire;
-        this.listeHoraire = listeHoraire;
-        listeHoraire.add(this);
+        ;
     }
 
     public int getIdHoraire() {
