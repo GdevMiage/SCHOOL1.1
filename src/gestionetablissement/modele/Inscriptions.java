d12201be624101c67c06bf7fe5e7afcabde0f690
salimaouali <salimaouali08@gmail.com>
2018-04-11 01:30:05 +0200

    inscription: integration  of  definirId, liste static

--- src/gestionetablissement/modele/Inscriptions.java
+++ src/gestionetablissement/modele/Inscriptions.java
@@ -3,7 +3,9 @@
 package gestionetablissement.modele;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 
  
 public class Inscriptions {
@@ -18,7 +20,49 @@ public class Inscriptions {
     private double creditInscription; 
     private ArrayList<Modules> listeAchat ;
     private ArrayList<Groupes> listeGroupes;
-    private ListPro<Inscriptions> listeInscriptions;
+    
+  public static final ListPro<Inscriptions> listeInscriptions = new ListPro<Inscriptions>();
+   
+    {
+        this.listeInscriptions.add(this);
+    }
+  
+        //pour definir a chaque fois les ID
+    public static int definirId(){
+        Iterator it = listeInscriptions.iterator();
+        
+        //année actuelle 
+       
+        int annee =Calendar.getInstance().get(Calendar.YEAR);
+        
+        Inscriptions InscriptionsPlusGrandId=null;
+        boolean vide =true;
+        if(it.hasNext())  InscriptionsPlusGrandId=  (Inscriptions) it.next();
+        while(it.hasNext()){
+            Inscriptions Inscriptions = (Inscriptions) it.next();
+            vide=false;
+            if(Inscriptions.getIdInscription()>InscriptionsPlusGrandId.getIdInscription()) InscriptionsPlusGrandId =Inscriptions;
+        }
+            
+            if(vide) {System.out.println((annee *10)+1);return ((annee *100000)+1);}
+            else {System.out.println(InscriptionsPlusGrandId.getIdInscription()+1);return InscriptionsPlusGrandId.getIdInscription()+1;}
+        
+        
+    }
+    
+    public static ArrayList<Inscriptions> of(String id ){
+        Iterator it = listeInscriptions.iterator();
+        ArrayList<Inscriptions> listRecherche=null;
+        while(it.hasNext()){
+            Inscriptions Inscriptions = (Inscriptions) it.next();
+            String idd = Inscriptions.getIdInscription()+"";
+            if(idd.indexOf(id)!=0){
+                
+                listRecherche.add(Inscriptions);
+            }
+        }
+        return listRecherche;
+    }
     
     public Inscriptions(){
         
@@ -39,8 +83,7 @@ public class Inscriptions {
         this.creditInscription = creditInscription;
         this.listeAchat=listeAchat;
         this.listeGroupes=listeGroupes;
-        this.listeInscriptions=listeInscriptions;
-        listeInscriptions.add(this);
+        
     }
 
     public int getIdInscription() {
