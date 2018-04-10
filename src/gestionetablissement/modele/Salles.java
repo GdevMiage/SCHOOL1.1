a753bfa9368f6c08d93526f79110750f52bd6399
salimaouali <salimaouali08@gmail.com>
2018-04-11 01:59:59 +0200

    salle : integration  of  definirId, liste static

--- src/gestionetablissement/modele/Salles.java
+++ src/gestionetablissement/modele/Salles.java
@@ -1,6 +1,10 @@
  
 package gestionetablissement.modele;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+
  
 public class Salles {
     
@@ -8,6 +12,49 @@ public class Salles {
     private String libelSalle;
     private int capaSalle;
     private ListPro<Salles> listeSalle;
+    
+    public static final ListPro<Salles> listeSalles = new ListPro<Salles>();
+   
+    {
+        this.listeSalles.add(this);
+    }
+  
+        //pour definir a chaque fois les ID
+    public static int definirId(){
+        Iterator it = listeSalles.iterator();
+        
+        //année actuelle 
+       
+        int annee =Calendar.getInstance().get(Calendar.YEAR);
+        
+        Salles SallesPlusGrandId=null;
+        boolean vide =true;
+        if(it.hasNext())  SallesPlusGrandId=  (Salles) it.next();
+        while(it.hasNext()){
+            Salles Salles = (Salles) it.next();
+            vide=false;
+            if(Salles.getIdSalle()>SallesPlusGrandId.getIdSalle())SallesPlusGrandId = Salles;
+        }
+            
+            if(vide) {System.out.println((annee *10)+1);return ((annee *100000)+1);}
+            else {System.out.println(SallesPlusGrandId.getIdSalle()+1);return SallesPlusGrandId.getIdSalle()+1;}
+        
+        
+    }
+    
+    public static ArrayList<Salles> of(String id ){
+        Iterator it = listeSalles.iterator();
+        ArrayList<Salles> listRecherche=null;
+        while(it.hasNext()){
+            Salles Salles = (Salles) it.next();
+            String idd = Salles.getIdSalle()+"";
+            if(idd.indexOf(id)!=0){
+                
+                listRecherche.add(Salles);
+            }
+        }
+        return listRecherche;
+    }
     public Salles(){
         
     }
